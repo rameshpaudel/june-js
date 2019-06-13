96682bac3582a86a87c062b362b0c5e360639e46
Ramesh Paudel <rpaudel58@gmail.com>
2019-06-13 16:56:58 +0545

    Added comments to functions

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,9 @@
+//Functions are used to encapsulate certain functionalities
 
+//They help us to reuse our code
+
+//The a,b,c are the parameters that are passed to the function
+//The equals to oprator assigns the default value if the value is not passed as parameter
 function sum(a, b, c = 0, d = 0, e = 0) {
     return a + b + c + d + e;
 
@@ -9,6 +14,8 @@ function substract(a, b) {
     return a - b;
 }
 
+//Asks user for correct input
+//Runs infinitly until the user inputs integer
 function askUserForCorrectInput(){
     var numOfUsers = prompt("ENter the no of users");
 
@@ -21,7 +28,7 @@ function askUserForCorrectInput(){
     return conversionToInt;
 }
 
-
+//Checks to see if the given parameter is indeed an integer
 function checkIfInteger(data){
     if (isNaN(parseInt(data))) {
         return false;
@@ -34,7 +41,7 @@ var test1 = checkIfInteger(20);
 var test2 = checkIfInteger("asdkfj");
 
 
-
+// Ask the user for student datas
 function getStudentData() {
     var numOfUsers = askUserForCorrectInput()
     var nameList = [];
