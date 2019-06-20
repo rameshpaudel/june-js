da534c11812000327c3d3ae0a6a4b59905468d29
Ramesh Paudel <rpaudel58@gmail.com>
2019-06-20 15:56:34 +0545

    Added array loops, rest spread operator

--- js/array2.js
+++ js/array2.js
@@ -1,7 +1,7 @@
 var arr = [1,2,3,4,5,6,7,8,9,10];
 
 for(var i = 0; i < arr.length; i++){
-    console.log(i)//, arr[i]);         
+    // console.log(i)//, arr[i]);         
 }
 
 function mapEveryItem(value,index){
@@ -45,4 +45,59 @@ var studentList = [
 var students = studentList.filter(function(value,index){
     return value.age < 25
 })
-console.log(students);
+// console.log(students);
+
+function mappings(value,index){
+    return {
+        name: value.name,
+        age: value.age *5
+    }
+}
+
+var mappedData = studentList.map(mappings)
+
+
+var loopData = studentList.forEach(function(value,index){
+    console.log(value.age)
+})
+
+
+var names = ['John','Jane','June','Joe','Harry'];
+
+
+var searchData = names.find(function(value,index){
+    return value == "test"
+})
+
+
+
+
+var multiArr = [
+    [1,2,3],
+    [2,3,4],
+    [3,4,5]
+]
+
+var multiLoopData = multiArr.forEach(function(value,index){
+
+    let mappedData = value.map(function(data,position){
+        return data * 2;
+    })
+
+    console.log(mappedData);
+})
+
+
+
+//Rest spread operator => ...
+//ARRAY Destructuring
+
+var allData = [...arr,...names]
+
+//Objects
+
+var allObjs = {
+    occupation: "Student",
+    dob: '2019-10-12',
+    ...studentList[0],
+}
---
+++ js/test.js
@@ -0,0 +1,24 @@
+console.log('The value in the test variable is' , test);
+
+var test = "Some value will be placed here later on";
+
+var theNumber = 10;
+
+var arr = [1,2,3,4,5,6,7,8];
+
+var obj = {
+    name: "John",
+    surname: "Smith",
+    age: 24
+}
+
+
+console.warn(test);
+
+console.error(test);
+
+
+console.log("Test ", test)
+
+// var askUser = prompt("Enter your name");
+// alert("Hello World")
